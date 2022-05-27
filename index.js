eb51b0ac6e4a5ce310c1e296fb45a4a865fb7a9a
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-05-27 22:01:55 +0600

    trying again

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const port = process.env.PORT || 5000;
 // app.use(cors(corsConfig));
 // app.options('*', cors(corsConfig));
 
-app.use(cors());
-// app.use(
-//     cors({
-//         origin: true,
-//         optionsSuccessStatus: 200,
-//         credentials: true,
-//     })
-// );
+// app.use(cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 
 
 app.use(express.json());
